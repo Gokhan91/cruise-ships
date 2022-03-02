1b35517297ae691f6ebcd5c686080aad82b60990
Gokhan91 <g.sancar91@gmail.com>
2022-03-02 20:49:39 +0200

    meaningful message

---
+++ src/cruise.js
@@ -0,0 +1,11 @@
+class Ship {
+    constructor(shipName, startingPort, noOfPassengers) {
+        this.shipName = shipName
+        this.startingPort = startingPort
+        this.noOfPassengers = noOfPassengers
+    }
+}
+
+module.exports = Ship;
+
+
---
+++ test/cruise.test.js
@@ -0,0 +1,20 @@
+const Ship = require('../src/cruise.js');
+
+//1 ship classı olacak
+//passenger ve starting portu olacak
+// shipe passenger ekleyebileceyik
+
+describe('constructor', () => {
+    it('returns an object', () => {
+        let gurrada = new Ship('gurrada', 'girne', 0);
+        expect(gurrada).toBeInstanceOf(Object)
+    });
+    it('has a starting port', () => {
+        let gurrada = new Ship('gurrada', 'girne', 0);
+        expect(gurrada.startingPort).toBe('girne');
+    });
+    it('has passengers', () => {
+        let gurrada = new Ship('gurrada', 'girne', 5);
+        expect(gurrada.noOfPassengers).toBe(5);
+    });
+});
